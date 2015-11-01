18e30984007d46951c29148f2eb5269144783664
Matthew Lanier <xorobabel@gmail.com>
2015-10-31 20:40:09 -0500

    Added pagination to blog entries

--- dagbok/__init__.py
+++ dagbok/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from views.admin import admin
 from models import Entry
 
@@ -10,5 +10,21 @@ app.debug = True
 @app.route("/")
 def home():
     main_entry = Entry.query.order_by(Entry.pub_date.desc()).first()
-    return render_template('home.html', main_entry=main_entry, title = "Home")
+    entries = Entry.query.order_by(Entry.pub_date.desc()).limit(4).offset(1)
+    has_next = (Entry.query.count() > 5)
+    next_page = 2
+    return render_template('home.html', main_entry=main_entry, entries=entries,
+                           has_next=has_next, next_page=next_page, title = "Home")
+
+@app.route("/page/<int:page_num>")
+def page(page_num):
+    if (page_num == 1):
+        return redirect(url_for('home'))
+    offset = 5 + ((page_num - 2) * 5)
+    entries = Entry.query.order_by(Entry.pub_date.desc()).limit(5).offset(offset)
+    has_next = (Entry.query.count() > (offset + 5))
+    next_page = page_num + 1
+    prev_page = page_num - 1
+    return render_template('blog_page.html', entries=entries, has_next=has_next,
+                           next_page=next_page, prev_page=prev_page, title = "Blog")
 
