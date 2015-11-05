193d2a2a84cf10f1e55cfd79454340dd1dc09223
Matthew Lanier <mclanier@ptci.com>
2015-11-05 14:20:27 -0600

    Removing User model

--- dagbok/models.py
+++ dagbok/models.py
@@ -3,19 +3,6 @@ from database import Base
 from datetime import datetime
 import re
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    email = Column(String(120), unique=True)
-
-    def __init__(self, name=None, email=None):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % (self.name)
-
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
